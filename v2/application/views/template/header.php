6172b3427e8eec692b10c4d3d916c40e36fe4166
kadek sastrawan <dexas90@gmail.com>
2013-08-31 20:22:56 +0800

    tutup yg tidak perlu
    tutup yg tidak perlu

--- v2/application/views/template/header.php
+++ v2/application/views/template/header.php
@@ -67,10 +67,12 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <!--
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+	-->
 </head>
 
 <!--[if lt IE 7 ]> <body class="ie ie6"> <![endif]-->
